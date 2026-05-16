375b3b5b9bce055d5711d7c19efb4298b653c410
sahinmallick14 <sahinmallick14@gmail.com>
2026-05-16 18:57:53 +0530

    fix

--- frontend/utils/axios.js
+++ frontend/utils/axios.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL ?? '/api/v1'
 
 export const axiosInstance = axios.create({
-    baseURL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
     withCredentials: true,
 });
 
